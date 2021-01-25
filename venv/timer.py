1d80bb9c544c72bbb707b94327be56140bb851e1
davsuar <deltacompu@hotmail.com>
2021-01-25 07:59:34 -0800

    Version is fully functional

--- venv/timer.py
+++ venv/timer.py
@@ -12,17 +12,17 @@ while True:
     current_time=now.strftime("%H:%M")
     time.sleep(10)
     print(current_time)
-    if current_time == "07:25" and firstShift:
+    if current_time == "08:00" and firstShift:
         print('Running script first shift')
         start.sTART()
         firstShift = False
         secondShift = True
-    elif current_time == "07:27" and secondShift:
+    elif current_time == "15:00" and secondShift:
         print('Running script second shift 2')
         start.sTART()
         secondShift=False
         thirdShift=True
-    elif current_time=="07:29" and thirdShift:
+    elif current_time=="23:00" and thirdShift:
         print('Running script third shift 3')
         start.sTART()
         thirdShift=False
